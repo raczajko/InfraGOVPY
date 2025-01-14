79e7e8368b483f446fca2f0a0c6963f4c221eefd
Raul Aguiar <raczajko@gmail.com>
2025-01-14 13:04:53 -0500

    automate lists pull

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+cd $blupdate
+echo "$(pwd)"
 echo "creando branch"
 fecha="$(date +"%Y%m%d")"
 git checkout -b $fecha origin/main 
 
 echo "refrescando listas..."
-wget https://cinsarmy.com/list/ci-badguys.txt
-wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4
-wget https://iplists.firehol.org/files/firehol_level1.netset
-wget https://iplists.firehol.org/files/firehol_level2.netset
-wget https://iplists.firehol.org/files/firehol_level3.netset
+wget https://cinsarmy.com/list/ci-badguys.txt -O ci-badguys.txt
+wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4 -O abuseipdb-s100-30d.ipv4
+wget https://iplists.firehol.org/files/firehol_level1.netset -O firehol_level1.netset
+wget https://iplists.firehol.org/files/firehol_level2.netset -O firehol_level2.netset
+wget https://iplists.firehol.org/files/firehol_level3.netset -O firehol_level3.netset
 
 echo "actualizando repositorio..."
-git add .
-git commit -m "update"$fecha
-git checkout main
-git merge --ff-only origin/$fecha
-git push origin main
+#git add .
+#git commit -m "update"$fecha
+#git checkout main
+#git reset origin/$fecha
+#git push origin main
