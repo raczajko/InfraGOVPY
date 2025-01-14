d11be84d3bc7df1eaeb2d793a5ca6d4ed5c47777
Raul Aguiar <raczajko@gmail.com>
2025-01-14 12:50:04 -0500

    update20250114

---
+++ blacklists/automate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "creando branch"
+fecha="$(date +"%Y%m%d")"
+git checkout -b $fecha origin/main 
+
+echo "refrescando listas..."
+wget https://cinsarmy.com/list/ci-badguys.txt
+wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4
+wget https://iplists.firehol.org/files/firehol_level1.netset
+wget https://iplists.firehol.org/files/firehol_level2.netset
+wget https://iplists.firehol.org/files/firehol_level3.netset
+
+echo "actualizando repositorio..."
+git add .
+git commit -m "update"$fecha
+git push origin $fecha
