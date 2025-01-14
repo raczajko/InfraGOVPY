f1e13100791b1cc8af7dce20f98a1936044d0fce
Raul Aguiar <raul.aguiar@webpt.com>
2025-01-14 16:02:16 -0300

    creada carpeta sources

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -6,18 +6,20 @@ fecha="$(date +"%Y%m%d%H%M")"
 git checkout -b $fecha origin/main 
 
 echo "refrescando listas..."
-wget https://cinsarmy.com/list/ci-badguys.txt -O ci-badguys.txt
-wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4 -O abuseipdb-s100-30d.ipv4
-wget https://iplists.firehol.org/files/firehol_level1.netset -O firehol_level1.netset
-wget https://iplists.firehol.org/files/firehol_level2.netset -O firehol_level2.netset
-wget https://iplists.firehol.org/files/firehol_level3.netset -O firehol_level3.netset
+wget https://cinsarmy.com/list/ci-badguys.txt -O sources/ci-badguys.txt
+wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4 -O sources/abuseipdb-s100-30d.ipv4
+wget https://iplists.firehol.org/files/firehol_level1.netset -O sources/firehol_level1.netset
+wget https://iplists.firehol.org/files/firehol_level2.netset -O sources/firehol_level2.netset
+wget https://iplists.firehol.org/files/firehol_level3.netset -O sources/firehol_level3.netset
 
 echo "generando bundle..."
-tail -n +14 abuseipdb-s100-30d.ipv4 | cut -d '#' -f 1 > todos.txt
-tail -n +34 firehol_level1.netset >> todos.txt
-tail -n +32 firehol_level2.netset >> todos.txt
-tail -n +35 firehol_level3.netset >> todos.txt
+tail -n +14 sources/abuseipdb-s100-30d.ipv4 | cut -d '#' -f 1 > todos.txt
+tail -n +34 sources/firehol_level1.netset >> todos.txt
+tail -n +32 sources/firehol_level2.netset >> todos.txt
+tail -n +35 sources/firehol_level3.netset >> todos.txt
 cat ci-badguys.txt >> todos.txt
+
+echo "eliminando duplicados"
 sort -u todos.txt > listado_full.txt
 rm todos.txt
 
