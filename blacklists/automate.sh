8741f57afaa373f1e90f3405933380ccb978a20c
Raul Aguiar <raczajko@gmail.com>
2025-01-17 10:05:11 -0300

    update202501171000

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -27,8 +27,7 @@ tail -n +37 sources/botscout_7d.ipset >> todos.txt
 cat sources/blocklist_de_all.txt >> todos.txt
 cat sources/ci-badguys.txt >> todos.txt
 echo "eliminando duplicados"
-iprange -J todos.txt > listado_full.txt
-rm -f todos.txt
+iprange --print-ranges todos.txt > listado_full.txt
 
 #listado_fail2ban
 tail -n +14 sources/abuseipdb-s100-30d.ipv4 | cut -d '#' -f 1 > todosf2b.txt
@@ -37,7 +36,6 @@ tail -n +37 sources/botscout_7d.ipset >> todosf2b.txt
 cat sources/blocklist_de_all.txt >> todosf2b.txt
 cat sources/ci-badguys.txt >> todosf2b.txt
 iprange --print-single-ips todosf2b.txt > listado_fail2ban.txt
-rm -f todosf2b.txt
 
 echo "generando listado para Forti...."
 split -a 3 -l 300000 listado_full.txt listado_forti_
@@ -45,6 +43,9 @@ split -a 3 -l 300000 listado_full.txt listado_forti_
 echo "generando md5sum de los listados..."
 md5sum listado_full.txt > listado_full.txt.md5
 md5sum listado_fail2ban.txt > listado_fail2ban.txt.md5
+
+echo "limpiando archivos temporales"
+rm -f todos.txt todosf2b.txt
 
 echo "actualizando repositorio..."
 git add -A
