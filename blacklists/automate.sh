85c9e138fac0e993a69f90910fe6d2b3a591371a
Raul Aguiar Czajkowski <raczajko@gmail.com>
2026-06-18 07:27:34 -0300

    reduciendo threshold de 30 a 7 dias

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -9,11 +9,11 @@ git checkout -b $fecha origin/main
 
 echo "refrescando listas..."
 wget https://cinsarmy.com/list/ci-badguys.txt -O sources/ci-badguys.txt
-wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-30d.ipv4 -O sources/abuseipdb-s100-30d.ipv4
+wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-7d.ipv4 -O sources/abuseipdb-s100-7d.ipv4
 wget https://iplists.firehol.org/files/firehol_level1.netset -O sources/firehol_level1.netset
 wget https://iplists.firehol.org/files/firehol_level2.netset -O sources/firehol_level2.netset
-wget https://iplists.firehol.org/files/firehol_level3.netset -O sources/firehol_level3.netset
-wget https://iplists.firehol.org/files/firehol_abusers_30d.netset -O sources/firehol_abusers_30d.netset
+#wget https://iplists.firehol.org/files/firehol_level3.netset -O sources/firehol_level3.netset
+wget https://iplists.firehol.org/files/firehol_abusers_1d.netset -O sources/firehol_abusers_1d.netset
 wget https://iplists.firehol.org/files/botscout_7d.ipset -O sources/botscout_7d.ipset
 wget https://lists.blocklist.de/lists/all.txt -O sources/blocklist_de_all.txt
 rm -Rf listado*
@@ -22,11 +22,11 @@ echo "generando bundle..."
 #agregando feed IOC
 cat ../feeds/legacyhunt-24h.csv | cut -d ',' -f 1 >> infra_govpy.txt
 #listado_full
-tail -n +14 sources/abuseipdb-s100-30d.ipv4 | cut -d '#' -f 1 > todos.txt
+tail -n +14 sources/abuseipdb-s100-7d.ipv4 | cut -d '#' -f 1 > todos.txt
 tail -n +34 sources/firehol_level1.netset >> todos.txt
 tail -n +32 sources/firehol_level2.netset >> todos.txt
-tail -n +35 sources/firehol_level3.netset >> todos.txt
-tail -n +32 sources/firehol_abusers_30d.netset >> todos.txt
+#tail -n +35 sources/firehol_level3.netset >> todos.txt
+tail -n +32 sources/firehol_abusers_1d.netset >> todos.txt
 tail -n +37 sources/botscout_7d.ipset >> todos.txt
 cat sources/blocklist_de_all.txt >> todos.txt
 cat sources/ci-badguys.txt >> todos.txt
@@ -35,7 +35,7 @@ echo "eliminando duplicados"
 /usr/bin/iprange --print-ranges todos.txt > listado_full.txt
 
 #listado_fail2ban
-tail -n +14 sources/abuseipdb-s100-30d.ipv4 | cut -d '#' -f 1 > todosf2b.txt
+tail -n +14 sources/abuseipdb-s100-7d.ipv4 | cut -d '#' -f 1 > todosf2b.txt
 tail -n +35 sources/firehol_level3.netset >> todosf2b.txt
 tail -n +37 sources/botscout_7d.ipset >> todosf2b.txt
 cat sources/blocklist_de_all.txt >> todosf2b.txt
