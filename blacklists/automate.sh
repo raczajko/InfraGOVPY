541aa379ff1e4ee0a77c8c71e27fa3be91281b28
Raul Aguiar Czajkowski <raczajko@gmail.com>
2026-07-07 09:55:30 -0300

    eliminar firehol temporalmente

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -10,8 +10,8 @@ git checkout -b $fecha origin/main
 echo "refrescando listas..."
 wget https://cinsarmy.com/list/ci-badguys.txt -O sources/ci-badguys.txt
 wget https://github.com/borestad/blocklist-abuseipdb/raw/refs/heads/main/abuseipdb-s100-7d.ipv4 -O sources/abuseipdb-s100-7d.ipv4
-wget https://iplists.firehol.org/files/firehol_level1.netset -O sources/firehol_level1.netset
-wget https://iplists.firehol.org/files/firehol_level2.netset -O sources/firehol_level2.netset
+#wget https://iplists.firehol.org/files/firehol_level1.netset -O sources/firehol_level1.netset
+#wget https://iplists.firehol.org/files/firehol_level2.netset -O sources/firehol_level2.netset
 #wget https://iplists.firehol.org/files/firehol_level3.netset -O sources/firehol_level3.netset
 wget https://iplists.firehol.org/files/firehol_abusers_1d.netset -O sources/firehol_abusers_1d.netset
 wget https://iplists.firehol.org/files/botscout_7d.ipset -O sources/botscout_7d.ipset
