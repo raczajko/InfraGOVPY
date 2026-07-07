a9884d658ca3178d3ccecdf7a4698cc65abe79d6
Raul Aguiar Czajkowski <raczajko@gmail.com>
2026-07-07 09:56:42 -0300

    eliminar firehol temporalmente

--- blacklists/automate.sh
+++ blacklists/automate.sh
@@ -24,8 +24,8 @@ cat ../feeds/legacyhunt-24h.csv | cut -d ',' -f 1 >> infra_govpy.txt
 #listado_full
 tail -n +2 sources/iocs-infra-govpy.txt > todos.txt
 tail -n +14 sources/abuseipdb-s100-7d.ipv4 | cut -d '#' -f 1 >> todos.txt
-tail -n +34 sources/firehol_level1.netset >> todos.txt
-tail -n +32 sources/firehol_level2.netset >> todos.txt
+#tail -n +34 sources/firehol_level1.netset >> todos.txt
+#tail -n +32 sources/firehol_level2.netset >> todos.txt
 #tail -n +35 sources/firehol_level3.netset >> todos.txt
 tail -n +32 sources/firehol_abusers_1d.netset >> todos.txt
 tail -n +37 sources/botscout_7d.ipset >> todos.txt
